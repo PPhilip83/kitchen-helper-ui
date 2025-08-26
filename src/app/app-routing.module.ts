485b887b960cd8e1046c11653ea60e012ca47297
Paige <paigephilippart@gmail.com>
2025-08-26 11:22:49 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShellComponent } from './layout/shell/shell.component';
+import { IngredientsComponent } from './pages/ingredients/ingredients.component';
+import { RecipesComponent } from './pages/recipes/recipes.component';
+import { FinderComponent } from './pages/finder/finder.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      { path: '', redirectTo: 'ingredients', pathMatch: 'full' },
+      { path: 'ingredients', component: IngredientsComponent },
+      { path: 'recipes', component: RecipesComponent },
+      { path: 'finder', component: FinderComponent },
+    ],
+  },
+  { path: '**', redirectTo: 'ingredients' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ShellComponent } from './layout/shell/shell.component';
+import { IngredientsComponent } from './pages/ingredients/ingredients.component';
+import { RecipesComponent } from './pages/recipes/recipes.component';
+import { FinderComponent } from './pages/finder/finder.component';
+import { AddIngredientDialogComponent } from './shared/add-ingredient-dialog/add-ingredient-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShellComponent,
+    IngredientsComponent,
+    RecipesComponent,
+    FinderComponent,
+    AddIngredientDialogComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
